fb6606d12f067f283f0a673de194a90eb8598a19
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-06-30 11:12:01 +0300

    push

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+// document.addEventListener('DOMContentLoaded', function submitData(name, email){
+
+//     fetch('http://localhost:3000/users', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             Accept: 'application/json',
+//         },
+//         body: JSON.stringify({
+//             name,
+//             email,
+//         })
+//     })
+//     .then(resp => resp.json())
+//     .then(data => console.log(data))
+// });
+
+
+function submitData(name, email){
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({
+            name,
+            email,
+        })
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+    .catch(error)
+        console.error("Error:", error);
+}
+
+
